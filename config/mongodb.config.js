7c8cd73fab8c39e1bd81aa66a82eaf5280762b6e
saurav25400 <86851522+saurav25400@users.noreply.github.com>
2024-01-11 18:05:50 +0530

    Update mongodb.config.js

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 // const url="mongodb://localhost:27017/postaway"
 export const connectionUsingMongoose=async()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/postaway",{
+        await mongoose.connect(process.env.DB_URL,{
             useNewUrlParser:true,
             useUnifiedTopology: true
         })
